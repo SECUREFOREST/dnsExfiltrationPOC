3ac351ef67a2d7b4ef91582c86aa03a4e03eb192
Vadim Toptunov <vadim@secureforest.com>
2023-01-15 12:52:56 +0200

    Init version

---
+++ index.js
@@ -0,0 +1,65 @@
+const dgram = require("dgram");
+const server = dgram.createSocket("udp4");
+const dnsPacket = require("dns-packet");
+const fs = require("fs");
+const FilenameRegex = /(\S+)\.fsub\.secureforest\.com/;
+const DataRegex = /(\S+)\.dsub\.secureforest\.com/;
+const UDP_IP = "0.0.0.0";
+const UDP_PORT = 53;
+
+let fileName = "untagged.txt";
+
+const logger = fs.createWriteStream(fileName, { flags: "a" });
+server.bind(UDP_PORT, UDP_IP);
+
+function createReplyBuffer(id, name, reply) {
+  try {
+    return dnsPacket.encode({
+      type: "response",
+      id,
+      answers: [
+        {
+          type: "A",
+          class: "IN",
+          name,
+          ttl: 300,
+          data: reply,
+        },
+      ],
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function decodeBase64(data) {
+  let buff = new Buffer.from(data, "base64");
+  let text = buff.toString("ascii");
+  return text;
+}
+
+server.on("message", (msg, rinfo) => {
+  try {
+    // console.log(rinfo)
+    const dnsData = dnsPacket.decode(msg); // console.log(dnsData)
+    const dnsName = dnsData.questions[0].name;
+    const matchData = DataRegex.exec(dnsName);
+    const matchFilename = FilenameRegex.exec(dnsName);
+    if (matchFilename) {
+      const decoded = decodeBase64(matchFilename[1] + "=");
+      fileName = decoded; // console.log("dns req data:", match[1]);
+      const buf = createReplyBuffer(dnsData.id, dnsName, "8.8.8.8");
+      server.send(buf, rinfo.port, rinfo.address);
+    }
+    if (matchData) {
+      const decoded = decodeBase64(matchData[1] + "=");
+      fs.writeFileSync("./files/" + fileName, decoded + "\n"); // console.log("dns req data:", match[1]);
+      const buf = createReplyBuffer(dnsData.id, dnsName, "8.8.8.8");
+      server.send(buf, rinfo.port, rinfo.address);
+    }
+  } catch (e) {
+    console.log(e);
+    const buf = createReplyBuffer("8.8.4.4");
+    server.send(buf, rinfo.port, rinfo.address);
+  }
+});
